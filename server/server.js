0fecba771540ea454efa5798c03cadb79bd011a0
bwaobikeze <bwaobikeze@hotmail.com>
2024-07-13 05:52:05 -0500

    updated server file

--- server/server.js
+++ server/server.js
@@ -2,19 +2,31 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 5000;
-const db = require("../config/db");
+const db = require("./config/db");
 
 app.use(cors());
 app.use(express.json());
 
+// Get all points
 app.get("/api", async (req, res) => {
-    try { 
-        const { rows } = await db.query('SELECT * FROM point');
+  try {
+    const { rows } = await db.query("SELECT * FROM point");
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get a single point
+app.get("/api/:id", async (req, res) => { 
+    try {
+        const { rows } = await db.query("SELECT * FROM point WHERE id = $1", [req.params.id]);
         res.status(200).json(rows);
     } catch (error) {
         res.status(500).json(error);
     }
 });
+
 
 
 app.listen(port, () => {
