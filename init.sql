c6120d30535ae154ad59f00a6933e3cc350bf23a
bwaobikeze <bwaobikeze@hotmail.com>
2024-07-14 22:04:15 -0500

    implemented docker files & updated config file & added the clients package-lock file

---
+++ init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE point (
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL,
+    x integer NOT NULL,
+    y integer NOT NULL
+);
--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,13 @@
 const { Pool } = require("pg");
-const dotenv = require("dotenv");
+require("dotenv").config();
 
-dotenv.config();
-
+// Create a new pool here using the connection string below
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "plainpoints",
-  password: "Kindboy30@",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Export the query method for passing queries to the pool
